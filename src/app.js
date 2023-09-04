56787c71201298717e21447032a8bb8e18df99f1
SergioJaramilloL <sergiojaramilloluna@gmail.com>
2023-09-04 09:35:43 -0500

    add semicolons

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
 
-const configExpress = require('./config/express')
+const configExpress = require('./config/express');
 const routes = require('./routes');
 
 const app = express();
